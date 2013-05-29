2f5f475f8f557c57b9711e1550235f8059148dc9
Peter Fern <github@obfusc8.org>
2013-05-29 23:01:36 +1000

    Add `insecure` key containing upgradeable packages from `security` repos

--- files/default/plugins/debian/system_packages.rb
+++ files/default/plugins/debian/system_packages.rb
@@ -1,9 +1,12 @@
 provides 'system_packages'
 
+require_plugin "linux::lsb"
+
 system_packages Mash.new unless system_packages
 system_packages['holding'] ||= Array.new
 system_packages['upgradeable'] ||= Mash.new
 system_packages['installed'] ||= Mash.new
+system_packages['insecure'] ||= Array.new
 
 # Get packages which are on hold or are upgradeable
 popen4("apt-get -q -s -f dist-upgrade") do |pid, stdin, stdout, stderr|
@@ -35,6 +38,25 @@ popen4("apt-get -q -s -f dist-upgrade") do |pid, stdin, stdout, stderr|
     when :upgradeable
       next unless line =~ /^Inst (\S+) \[.+\] \((\S+) /
       system_packages['upgradeable'][$1] = $2
+    end
+  end
+end
+
+# Get packages which are upgradeable and come from security
+popen4("apt-cache -qq showpkg #{system_packages['upgradeable'].keys.join(' ')}") do |pid, stdin, stdout, stderr|
+  stdin.close
+
+  package, version = nil, nil
+  stdout.each do |line|
+    line.chomp!
+    if line =~ /^Package: (\S+)$/
+      package = $1
+      version = system_packages['upgradeable'][package]
+    elsif version and line =~ /^#{version}\s+\(/
+      paths = line.split(/\s+/).drop(1)
+      if paths.any? {|p| p =~ /_#{lsb[:codename]}-security_/}
+        system_packages['insecure'] << package
+      end
     end
   end
 end
